68ee7ff849235d3c81f866b22df19a3a937c4fe1
Joe Cox <BootBlock@users.noreply.github.com>
2023-06-22 09:54:11 +0100

    Add FlickeringLight
    Adds a flickering light effect. Needs fine-tuning.

---
+++ Objects/FlickeringLight.gd
@@ -0,0 +1,22 @@
+# FlickeringLight.gd
+extends PointLight2D
+
+@export var noise: NoiseTexture2D
+
+var time_passed: float = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.time_passed += delta
+
+	var sampled_noise = abs(self.noise.noise.get_noise_1d(self.time_passed))
+	print(sampled_noise)
+	self.energy = 0.5 + sampled_noise
+
+	# Wrap the value so it doesn't become so large that it loses precision.
+	if self.time_passed > 1000000:
+		self.time_passed = 0
